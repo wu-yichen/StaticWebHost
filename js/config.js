4518f2dd3eee5e35df4d798142e20c144f56f6eb
wu-yichen <wuyichen1987@gmail.com>
2020-05-17 12:30:56 +0100

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "eu-west-1_69LooGW4Y", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "2iha13os0r9djvk2f3hcas249k", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
